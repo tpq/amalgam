13235c2a5893a9433dd4547df53f27dea9a5bba3
Thom Quinn <contacttomquinn@gmail.com>
2020-02-19 15:44:56 +1100

    Add weighted Aitchison distance function and test

---
+++ R/8-wadist.R
@@ -0,0 +1,61 @@
+#' Calculate Weighted Aitchison Distance
+#'
+#' This function computes the weighted aitchison distance proposed by
+#'  Greenacre & Lewi (2009) in \code{doi:10.1007/s00357-009-9027-y}.
+#'  Unlike the unweighted Aitchison distance, the weighted Aitchison
+#'  distance has distributional equivalence.
+#'
+#' @param A A matrix of compositional data.
+#' @return A matrix of distances.
+#' @export
+wadist <- function(A){
+
+  # close the column weights (per Greenacre & Lewi "for notational convenience")
+  col_weight <- colSums(A)
+  col_weight <- col_weight / sum(col_weight)
+
+  # logM is the (log of) the weighted geometric mean
+  # -- defined as sum(col_weight * log(x)) / sum(col_weight)
+  # -- but sum(col_weight) = 1 so we can ignore it
+  logM <- apply(A, 1, function(row) sum(col_weight * log(row)))
+
+  # like a CLR, we subtract the log geometric mean from the log data
+  centered <- sweep(log(A), 1, logM, "-")
+
+  # now, we use the centered data to calculate a weighted distance
+  newdf <- sweep(centered, 2, col_weight, function(x, y) x * sqrt(y))
+  as.matrix(stats::dist(newdf))
+}
+
+#' Calculate Fitness of Binary Vector
+#'
+#' This objective function seeks to maximize the correlation
+#'  between the weighted Aitchison distance of the complete composition
+#'  and the weighted Aitchison distance of the amalgamation.
+#'
+#' @param codon A binary vector.
+#' @param ARGS Handled by \code{\link{prepareArgs}}.
+#' @export
+objective.keepWeightedDist <- function(codon, ARGS){
+
+  W <- do.call(ARGS$weights, list(codon, n.amalgams = ARGS$n.amalgams))
+  A <- ARGS$x %*% W
+
+  # Don't allow zeros!
+  if(any(A == 0)){
+    return(-1000000)
+  }
+
+  # Calculate distance for SLR or amalgams
+  if(ARGS$asSLR){
+    stop("This objective function does not support summed log-ratios.")
+  }else{
+    NEWDIST <- wadist(A)
+  }
+
+  # Maximize correlation between AMALG dist and TARGET dist
+  DIST <- ARGS$TARGET
+  Od <- DIST[lower.tri(DIST)]
+  Ad <- NEWDIST[lower.tri(NEWDIST)]
+  stats::cor(Od, Ad)
+}
---
+++ tests/testthat/test-8-wadist.R
@@ -0,0 +1,18 @@
+# A is a matrix where columns 6 and 7 are identical
+library(amalgam)
+A <- randAcomp(5)
+A <- cbind(A, cbind(rep(.2, 5), rep(.2, 5)))
+A <- sweep(A, 1, rowSums(A), "/")
+
+# B is a matrix where columns 6 and 7 are merged
+B <- A
+B[,6] <- B[,6] + B[,7]
+B <- B[,-7]
+
+test_that("weighted Aitchison distance has distributional equivalence", {
+
+  expect_equal(
+    wadist(A),
+    wadist(B)
+  )
+})
